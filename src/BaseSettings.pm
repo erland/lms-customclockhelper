5721aa5319df0ef9b4ae3a831118d7a4bc5164ab
Erland Isaksson <erland@isaksson.info>
2017-07-20 11:11:26 +0200

    Removed license checks

--- src/BaseSettings.pm
+++ src/BaseSettings.pm
@@ -63,9 +63,9 @@ sub handler {
 	}
 	$params->{'subpages'} = \%currentSubPages;
 	$params->{'subpage'} = $class->currentPage($client,$params);
-	$params->{'licensemanager'} = Plugins::CustomClockHelper::Plugin::isPluginsInstalled($client,'LicenseManagerPlugin');
-	my $validateRequest = Slim::Control::Request::executeRequest($client,['licensemanager','validate','application:CustomClock']);
-	$params->{'licensed'} = $validateRequest->getResult("result");
+	$params->{'licensemanager'} = 1; #Plugins::CustomClockHelper::Plugin::isPluginsInstalled($client,'LicenseManagerPlugin');
+	#my $validateRequest = Slim::Control::Request::executeRequest($client,['licensemanager','validate','application:CustomClock']);
+	$params->{'licensed'} = 1; #$validateRequest->getResult("result");
 	return $class->SUPER::handler($client, $params);
 }
 		
